e774a260556ed1a878bafff11617a558f1a549cb
kewei <kewei@hoolai.com>
2013-04-28 17:17:08 +0800

    Initial Commit

---
+++ KKCallMe/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  KKCallMe
+//
+//  Created by Jackie on 13-4-28.
+//  Copyright (c) 2013年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ KKCallMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KKCallMe
+//
+//  Created by Jackie on 13-4-28.
+//  Copyright (c) 2013年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
